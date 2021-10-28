6fa8835e7f8983c4c49fd58119b3b0dd28db91ea
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2021-10-27 22:10:23 -0400

    Add files via upload

---
+++ quiz_game.js
@@ -0,0 +1,19 @@
+player_1_name = localStorage.getItem("player_1_name");
+player_2_name = localStorage.getItem("player_2_name");
+player_1_score=0;
+player_2_score=0;
+document.getElementById("player_1_name").innerHTML=player_1_name+":";
+document.getElementById("player_2_name").innerHTML=player_2_name+":";
+document.getElementById("player_1_score").innerHTML=player_1_score;
+document.getElementById("player_2_score").innerHTML=player_2_score;
+function send(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actualanwser=parseInt(number1)*parseInt(number2);
+
+    question_number="<h4>"+number1+"X"+number2+"</h4>";
+    input_box="<br>Answer:<input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn_info' onclick='check()'>Check</button>";
+    row=question_number+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+}
